0ce0f5b4b8d6176283e0f442df32a0e496c4a1db
Aosman4395 <ahamed.osman4395@gmail.com>
2026-05-02 19:16:06 +0100

    remove unused root public subnet variable

--- infra/main/variables.tf
+++ infra/main/variables.tf
@@ -63,8 +63,3 @@ variable "certificate_arn" {
   default     = "arn:aws:acm:eu-west-2:409987738946:certificate/43b4808a-aa96-4cb4-a27e-687336336cb3"
 }
 
-variable "public_subnet_ids" {
-  description = "The IDs of the public subnets for the Application Load Balancer"
-  type        = list(string)
-}
-
