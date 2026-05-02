074899c67f67b3c92111ee9e199e648a36407b64
Aosman4395 <ahamed.osman4395@gmail.com>
2026-05-02 17:30:50 +0100

    updated fmt

--- infra/main/provider.tf
+++ infra/main/provider.tf
@@ -11,12 +11,12 @@ provider "aws" {
   region = var.aws_region
 }
 
-terraform{
-backend "s3" {
-  bucket = var.state_bucket
-  key    = "eks-project/terraform.tfstate"
-  region = var.aws_region
-  use_lockfile = true
-  encrypt      = true
-}
+terraform {
+  backend "s3" {
+    bucket       = var.state_bucket
+    key          = "eks-project/terraform.tfstate"
+    region       = var.aws_region
+    use_lockfile = true
+    encrypt      = true
+  }
 }
